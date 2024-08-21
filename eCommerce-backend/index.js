ca1270fabd987b97e4e4e05a5fe9d35cf7af0ccf
Narotam-Mishra <narotam21jan@gmail.com>
2024-08-21 05:52:32 +0530

    full-stack-eCommerce_app_1.2_image-upload_api_added

--- eCommerce-backend/index.js
+++ eCommerce-backend/index.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
+const { type } = require('os');
 
 const app = express();
 
@@ -19,11 +20,91 @@ const mongo_URI = process.env.mongoURL;
 mongoose.connect(mongo_URI)
 
 // API creation
+app.get("/", (req, res) => {
+    res.send("Express App is running")
+})
+
+// image storage engine
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({ storage: storage})
+
+app.use('/images', express.static('upload/images'))
+
+// creating upload endpoint for images
+app.post('/upload', upload.single('product'), (req, res) => {
+    res.json({
+        success: 1,
+        image_url: `http://localhost:${portNo}/images/${req.file.filename}`
+    })
+})
+
+// schema for creating products
+const Product = mongoose.model("Product", {
+    id: {
+        type: Number,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    new_price:{
+        type: Number,
+        required: true,
+    },
+    old_price:{
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default: Date.now,
+    },
+    avilable:{
+        type: Boolean,
+        default: true,
+    },
+})
+
+app.post('/addProduct', async (req, res) => {
+    const product = new Product({
+        id: req.body.id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+    })
+
+    console.log("Product details:",product);
+    await product.save();
+    console.log("product saved into DB");
+    
+    res.json({
+        success: true,
+        name: req.body.name,
+    })
+})
+
 app.listen(portNo, (error) => {
     if(!error){
         console.log(`Server running on PORT: ${portNo}`);
     }else{
-        console.log("Error while connnecting MongoDB:", error);
+        console.log("Error while connnecting server:", error);
     }
 })
 
