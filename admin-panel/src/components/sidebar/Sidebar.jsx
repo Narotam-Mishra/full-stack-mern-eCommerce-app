ea0eec524b62a5df170fe0110be884bb83e54545
Narotam-Mishra <narotam21jan@gmail.com>
2024-08-23 23:50:38 +0530

    full-stack-eCommerce_app_1.6_Admin-Panel_Sidebar_added

---
+++ admin-panel/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,13 @@
+import './Sidebar.css'
+import { Link } from 'react-router-dom'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+        <Link to={}>
+        </Link>
+    </div>
+  )
+}
+
+export default Sidebar
