deacc405897087b110e8a0ea8c9f249377975dc3
Narotam-Mishra <narotam21jan@gmail.com>
2024-08-24 23:26:16 +0530

    full-stack-eCommerce_app_1.9_list-product_added

--- admin-panel/src/components/listProduct/ListProduct.jsx
+++ admin-panel/src/components/listProduct/ListProduct.jsx
@@ -1,9 +1,55 @@
+import { useEffect, useState } from 'react'
 import './ListProduct.css'
+import cross_icon from '../../assets/cross_icon.png'
 
 const ListProduct = () => {
+  const [allProducts, setAllProducts] = useState([]);
+
+  const fetchInfo = async () => {
+    await fetch('https://localhost:8421/allProducts').then((resp) => resp.json()).then((data) => {
+        setAllProducts(data);
+    })
+  }
+
+  useEffect(() => {
+    fetchInfo();
+  }, [])
+
   return (
     <div className='list-product'>
-        
+        <h1>All Products List</h1>
+        <div className="listproduct-format-main">
+            <p>Products</p>
+            <p>Title</p>
+            <p>Old Price</p>
+            <p>New Price</p>
+            <p>Category</p>
+            <p>Remove</p>
+        </div>
+        <div className="listproduct-allproducts">
+            <hr />
+            {
+                allProducts.map((product, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="listproduct-format-main listproduct-format"
+                      >
+                        <img
+                          src={product.image}
+                          alt=""
+                          className="listproduct-product-icon"
+                        />
+                        <p>{product.name}</p>
+                        <p>${product.old_price}</p>
+                        <p>${product.new_price}</p>
+                        <p>{product.category}</p>
+                        <img className='listproduct-remove-icon' src={cross_icon} alt="cross-icon" />
+                      </div>
+                    );
+                })
+            }
+        </div>
     </div>
   )
 }
