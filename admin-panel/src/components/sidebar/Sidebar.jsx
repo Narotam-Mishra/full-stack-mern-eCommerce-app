5a2e5dce04a7cad1bafd62727a67f4617cee27ee
Narotam-Mishra <narotam21jan@gmail.com>
2024-08-24 14:11:19 +0530

    full-stack-eCommerce_app_1.7_addProduct_and_listProduct_options_admin-panel

--- admin-panel/src/components/sidebar/Sidebar.jsx
+++ admin-panel/src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,23 @@
 import './Sidebar.css'
 import { Link } from 'react-router-dom'
+import add_product_icon from '../../assets/Product_Cart.svg'
+import list_product_icon from '../../assets/product_list_icon.svg'
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
-        <Link to={}>
+        <Link to={'/addProduct'} style={{textDecoration:"none"}}>
+          <div className="sidebar-item">
+            <img src={add_product_icon} alt="" />
+            <p>Add Product</p>
+          </div>
+        </Link>
+
+        <Link to={'/listProduct'} style={{textDecoration:"none"}}>
+          <div className="sidebar-item">
+            <img src={list_product_icon} alt="" />
+            <p>Product List</p>
+          </div>
         </Link>
     </div>
   )
